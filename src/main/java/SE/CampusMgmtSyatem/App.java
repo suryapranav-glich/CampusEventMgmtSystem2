6aa48e356f3e7daa51dec187a70e37e9fcda7894
bhargavikumbham <bhargavikumbham@kmit.in>
2025-09-04 23:09:23 +0530

    first commit

---
+++ src/main/java/SE/CampusMgmtSyatem/App.java
@@ -0,0 +1,13 @@
+package SE.CampusMgmtSyatem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
